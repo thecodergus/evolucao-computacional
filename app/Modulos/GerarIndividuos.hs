851149dcbf10d99e76b979144fceb638fae94331
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-06 13:46:24 -0300

    Adicionando Fitness

--- app/Modulos/GerarIndividuos.hs
+++ app/Modulos/GerarIndividuos.hs
@@ -12,14 +12,14 @@ gerarIndividuoBooleano :: Int -> IO (Individuo Bool)
 gerarIndividuoBooleano num_genes = do
   bools <- replicateM num_genes randomBool
   let intervalo' = Intervalo False True
-  return $ Individuo bools num_genes intervalo'
+  return $ Individuo bools num_genes intervalo' 0
 
 -- Função para gerar um individuo de tamanho N com alelos aleatórios entre (a, b)
 gerarIndividuoInteiroBound :: Int -> (Int, Int) -> IO (Individuo Int)
 gerarIndividuoInteiroBound num_genes por_entre = do
   inteiros <- replicateM num_genes (randomInt por_entre)
   let intervalo' = uncurry Intervalo por_entre
-  return $ Individuo inteiros num_genes intervalo'
+  return $ Individuo inteiros num_genes intervalo' 0
 
 -- Função para gerar um individuo de tamanho N com alelos aleatórios entre (a, b)
 gerarIndividuoInteiroPermutado :: Int -> IO (Individuo Int)
@@ -27,11 +27,11 @@ gerarIndividuoInteiroPermutado num_genes = do
   let por_entre = (1, num_genes)
   let inteiros = permutations [fst por_entre .. snd por_entre]
   let intervalo' = uncurry Intervalo por_entre
-  return $ Individuo (head inteiros) num_genes intervalo'
+  return $ Individuo (head inteiros) num_genes intervalo' 0
 
 -- Função para gerar um individuo de tamanho N com alelos aleatórios entre (a, b)
 gerarIndividuoFlutuante :: Int -> (Float, Float) -> IO (Individuo Float)
 gerarIndividuoFlutuante num_genes por_entre = do
   flutuantes <- replicateM num_genes (randomFloat por_entre)
   let intervalo' = uncurry Intervalo por_entre
-  return $ Individuo flutuantes num_genes intervalo'
+  return $ Individuo flutuantes num_genes intervalo' 0
--- app/Modulos/Tipos.hs
+++ app/Modulos/Tipos.hs
@@ -5,7 +5,8 @@ data Intervalo a = Intervalo {minimo :: a, maximo :: a} deriving (Show)
 data Individuo a = Individuo
   { genes :: [a],
     tamanho :: Int,
-    intervalo :: Intervalo a
+    intervalo :: Intervalo a,
+    fitness :: Float
   } deriving (Show)
 
 type Populacao a = [Individuo a]
