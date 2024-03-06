442ba503891fdcf325c02441758507f15a481ea2
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-06 15:41:38 -0300

    Simplificando linhas

--- app/Modulos/GerarPopulacao.hs
+++ app/Modulos/GerarPopulacao.hs
@@ -1,20 +1,19 @@
 module GerarPopulacao where
 
 
-import Tipos (Populacao)
+import Tipos (Populacao, Intervalo (..), Individuo (Individuo))
 import GerarIndividuos (gerarIndividuoBooleano, gerarIndividuoInteiroBound, gerarIndividuoFlutuante)
 import Control.Monad (replicateM)
+import Data.List (permutations)
 
 
 -- Função para gerar uma população de indivíduos booleanos
 gerarPopulacaoBooleana :: Int -> Int -> IO (Populacao Bool)
-gerarPopulacaoBooleana num_individuos num_genes = do
-  replicateM num_individuos (gerarIndividuoBooleano num_genes)
+gerarPopulacaoBooleana num_individuos num_genes = replicateM num_individuos (gerarIndividuoBooleano num_genes)
 
 -- Função para gerar uma população de indivíduos inteiros bounded
 gerarPopulacaoInteiroBound :: Int -> Int -> (Int, Int) -> IO (Populacao Int)
-gerarPopulacaoInteiroBound num_individuos num_genes intervalo = do
-  replicateM num_individuos (gerarIndividuoInteiroBound num_genes intervalo)
+gerarPopulacaoInteiroBound num_individuos num_genes intervalo = replicateM num_individuos (gerarIndividuoInteiroBound num_genes intervalo)
 
 -- Função para gerar uma população de indivíduos inteiros permutados
 gerarPopulacaoInteiroPermutado :: Int -> Int -> IO (Populacao Int)
@@ -25,7 +24,8 @@ gerarPopulacaoInteiroPermutado num_individuos num_genes = do
   let elementos = take num_individuos inteiros
   
   return $ map (\vetor -> Individuo vetor num_genes intervalo' 0) elementos
+
+
 -- Função para gerar uma população de indivíduos flutuantes
 gerarPopulacaoFlutuante :: Int -> Int -> (Float, Float) -> IO (Populacao Float)
-gerarPopulacaoFlutuante num_individuos num_genes intervalo = do
-  replicateM num_individuos (gerarIndividuoFlutuante num_genes intervalo)
+gerarPopulacaoFlutuante num_individuos num_genes intervalo = replicateM num_individuos (gerarIndividuoFlutuante num_genes intervalo)
