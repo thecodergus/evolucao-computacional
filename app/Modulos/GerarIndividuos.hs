ab1e875c922c737d1154777b1a81093ef77fae4b
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-13 15:10:10 -0300

    Deixando as funções puramente funconais e adicionando uma função para lidar com individuos para lidar com o problema SAT

--- app/Modulos/GerarIndividuos.hs
+++ app/Modulos/GerarIndividuos.hs
@@ -2,23 +2,26 @@ module GerarIndividuos where
 
 
 import GerarAleatoriedades
-    (randomBoolLista, randomIntLista, randomFloatLista)
+    (randomBoolLista, randomIntLista, randomFloatLista, randomListaDeListas)
 import Tipos ( Individuo(..))
 
 -- Função para gerar um individuo de tamanho N com alelos aleatórios booleanos
 gerarIndividuoBooleano :: Int -> IO (Individuo Bool)
-gerarIndividuoBooleano num_genes = do
-  bools <- randomBoolLista num_genes
-  return $ Individuo bools 0
+gerarIndividuoBooleano num_genes = 
+  Individuo <$> randomBoolLista num_genes <*> pure 0
 
 -- Função para gerar um individuo de tamanho N com alelos inteiros aleatórios entre (a, b)
 gerarIndividuoInteiroBound :: Int -> (Int, Int) -> IO (Individuo Int)
-gerarIndividuoInteiroBound num_genes por_entre = do
-  inteiros <- randomIntLista por_entre num_genes
-  return $ Individuo inteiros 0
+gerarIndividuoInteiroBound num_genes por_entre = 
+  Individuo <$> randomIntLista por_entre num_genes <*> pure 0
+
+-- Função auxliar para gerar um individuo que se adeque ao problema SAT
+gerarIndividuoInteiroForSAT :: Int -> (Int, Int) -> IO (Individuo [Int])
+gerarIndividuoInteiroForSAT num_genes por_entre = 
+  Individuo <$> randomListaDeListas por_entre num_genes <*> pure 0
 
 -- Função para gerar um individuo de tamanho N com alelos floats aleatórios entre (a, b)
 gerarIndividuoFlutuante :: Int -> (Float, Float) -> IO (Individuo Float)
-gerarIndividuoFlutuante num_genes por_entre = do
-  flutuantes <- randomFloatLista por_entre num_genes
-  return $ Individuo flutuantes  0
+gerarIndividuoFlutuante num_genes por_entre = 
+  Individuo <$> randomFloatLista por_entre num_genes <*> pure 0
+
