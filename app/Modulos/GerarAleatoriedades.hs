25e86ce2c4a57324a44ef758102447f8938cf722
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-13 10:50:09 -0300

    Add funções de aleatoriedades

--- app/Modulos/GerarAleatoriedades.hs
+++ app/Modulos/GerarAleatoriedades.hs
@@ -2,6 +2,7 @@ module GerarAleatoriedades where
 
 
 import System.Random ( randomRIO )
+import Control.Monad (replicateM)
 
 -- Função auxiliar para gerar um valor inteiro aleatório entre dois valores
 randomInt :: (Int, Int) -> IO Int
@@ -14,3 +15,11 @@ randomFloat = randomRIO
 -- Função auxiliar para gerar um valor booleano aleatório
 randomBool :: IO Bool
 randomBool = randomRIO (False, True)
+
+-- Função auxiliar para gerar um vetor de booleanos aleatorios
+randomBoolLista :: Int -> IO [Bool]
+randomBoolLista num = replicateM num randomBool
+
+-- Função auxiliar para gerar uma matriz de booleanos aleatorios
+randomBoolMatriz :: Int -> Int -> IO [[Bool]]
+randomBoolMatriz num_listas tam_lista = replicateM num_listas (randomBoolLista tam_lista)
