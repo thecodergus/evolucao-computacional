790fcabd51b5c7ba224d3bb51515b8d9bdf77d5f
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-13 16:12:21 -0300

    Adicionando função para encontrar o melhor individuo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 import GerarPopulacao (gerarPopulacaoForSAT)
 import Sat (resolver'')
+import Avaliacoes (maiorFitness)
 
 main :: IO ()
 main = do
@@ -9,3 +10,5 @@ main = do
   let pop_2 = map resolver'' pop
 
   print pop_2
+  print "Melhor avaliacao: "
+  print $ maiorFitness pop_2
