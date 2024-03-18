c9b50a051fd0295fffa2048416e24d6e2663d3cc
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-18 16:01:57 -0300

    Uninificando mais as funções

--- app/Modulos/Utils/Arquivo.hs
+++ app/Modulos/Utils/Arquivo.hs
@@ -1,23 +1,22 @@
 module Arquivo where
 import Data.Maybe (mapMaybe)
 
-stringToIntList :: String -> [Int]
-stringToIntList input = filter (/= 0) $ mapMaybe safeRead $ filter (not . null) $ concatMap words $ takeWhile ((/=) '%' . head) $ lines input
-  where
-    safeRead s = case reads s of
-                   [(x, "")] -> Just x
-                   _ -> Nothing
-
-
-stringsToIntLists :: [String] -> [[Int]]
-stringsToIntLists = map stringToIntList
 
 fileToIntLists :: FilePath -> IO [[Int]]
 fileToIntLists filePath = do
   content <- readFile filePath
   let lines' = drop 2 $ lines content -- Remove as duas primeiras linhas
   return $ removeEmptySublists $ stringsToIntLists lines'
+  where
+    stringsToIntLists :: [String] -> [[Int]]
+    stringsToIntLists = map stringToIntList
 
+    removeEmptySublists :: [[a]] -> [[a]]
+    removeEmptySublists = filter (not . null)
 
-removeEmptySublists :: [[a]] -> [[a]]
-removeEmptySublists = filter (not . null)
+    stringToIntList :: String -> [Int]
+    stringToIntList input = filter (/= 0) $ mapMaybe safeRead $ filter (not . null) $ concatMap words $ takeWhile ((/=) '%' . head) $ lines input
+      where
+        safeRead s = case reads s of
+                      [(x, "")] -> Just x
+                      _ -> Nothing
