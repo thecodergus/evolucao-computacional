9cf7af462dd906377b9e704616218c2f17110984
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-18 16:07:29 -0300

    Tava procurando elementos fora da fila

--- app/Modulos/Utils/Sat.hs
+++ app/Modulos/Utils/Sat.hs
@@ -1,10 +1,12 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
 module Sat where
 
 
-    
+
 replaceWithBools :: [[Int]] -> [Bool] -> [[Bool]]
 replaceWithBools intLists boolList = map (map (replace boolList)) intLists
   where
+    replace :: [Bool] -> Int -> Bool
     replace boolList i
-      | i < 0 = not (boolList !! abs (i))
-      | otherwise = boolList !! i
+      | i < 0 = not (boolList !! (abs i - 1))
+      | otherwise = boolList !! (i - 1)
