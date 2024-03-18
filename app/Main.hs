10e14ff9693c49d20a0353aca9486c17e48224ad
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-18 16:35:56 -0300

    Melhor individuo

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 import GerarPopulacao (gerarPopulacaoBooleana)
 import Arquivo (fileToIntLists)
-import Avaliacoes (avaliarSATs)
+import Avaliacoes (avaliarSATs, melhorIndividuo)
 
 main :: IO ()
 main = do
   pop <- gerarPopulacaoBooleana 10 100
-
   intLists <- fileToIntLists "/home/udesc/Documentos/evolucao-computacional-main/arquivoSAT.cnf"
   
-  print "Ola"
-  -- print intLists
+  print "Individuos:"
 
+  let individuos_avaliados = avaliarSATs pop intLists
 
-  -- let resultado  = replaceWithBools intLists (map genes pop)
-  print $ avaliarSATs pop intLists
+  print individuos_avaliados
+
+  print "Melhor individuo:"
+
+  print $ melhorIndividuo individuos_avaliados
