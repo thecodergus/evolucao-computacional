d9d0ef8e99d7c7acec53ed090e18bbe7663d72b4
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-18 16:16:50 -0300

    Tornando mais junto os codigos

--- app/Modulos/Avaliacoes.hs
+++ app/Modulos/Avaliacoes.hs
@@ -1,6 +1,5 @@
 module Avaliacoes where
 import Tipos (Individuo(genes, Individuo))
-import Sat (replaceWithBools)
 import Control.Parallel.Strategies (parMap, rpar)
 
 
@@ -13,6 +12,14 @@ avaliarSAT individuo disjuncao = Individuo (genes individuo) $ avaliar $ contaBo
         contaBools :: [Bool] -> (Int, Int)
         contaBools = foldl (\(trueCount, totalCount) x -> if x then (trueCount + 1, totalCount + 1) else (trueCount, totalCount + 1)) (0, 0)
 
+        replaceWithBools :: [[Int]] -> [Bool] -> [[Bool]]
+        replaceWithBools intLists boolList = map (map (replace boolList)) intLists
+            where
+                replace :: [Bool] -> Int -> Bool
+                replace boolList i
+                    | i < 0 = not (boolList !! (abs i - 1))
+                    | otherwise = boolList !! (i - 1)
 
+                    
 avaliarSATs :: [Individuo Bool] -> [[Int]] -> [Individuo Bool]
 avaliarSATs individuos disjuncao = parMap rpar (`avaliarSAT` disjuncao) individuos
--- app/Modulos/Utils/Sat.hs
+++ app/Modulos/Utils/Sat.hs
@@ -3,10 +3,3 @@ module Sat where
 
 
 
-replaceWithBools :: [[Int]] -> [Bool] -> [[Bool]]
-replaceWithBools intLists boolList = map (map (replace boolList)) intLists
-  where
-    replace :: [Bool] -> Int -> Bool
-    replace boolList i
-      | i < 0 = not (boolList !! (abs i - 1))
-      | otherwise = boolList !! (i - 1)
