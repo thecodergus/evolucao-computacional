f22254da4929472fb63c2045e9dbfda4d7ae6c04
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-03-25 14:39:09 -0300

    ADicionando comentarios

--- app/Modulos/Crosssover.hs
+++ app/Modulos/Crosssover.hs
@@ -5,16 +5,27 @@ import Tipos (Individuo(genes, Individuo))
 import GerarAleatoriedades (randomInt)
 
 
+-- A função 'crossoverUmPontoAleatorio' recebe dois indivíduos (pai e mãe) como entrada.
+-- Ela retorna um par de novos indivíduos que são o resultado do crossover de um ponto aleatório entre o pai e a mãe.
 crossoverUmPontoAleatorio :: Individuo (*) -> Individuo (*) -> IO (Individuo (*), Individuo (*))
-crossoverUmPontoAleatorio pai mae | length (genes pai) /= length (genes mae) = error "O tamanhos dos genes do pai e da mae devem ser iguais"
-                         | null (genes pai) = error "O numero de genes devem ser maiores que zero"
-                         | otherwise = crossover
+crossoverUmPontoAleatorio pai mae 
+  -- Verifica se o tamanho dos genes do pai e da mãe são iguais. Se não forem, lança um erro.
+  | length (genes pai) /= length (genes mae) = error "O tamanhos dos genes do pai e da mae devem ser iguais"
+  -- Verifica se a lista de genes do pai é vazia. Se for, lança um erro.
+  | null (genes pai) = error "O numero de genes devem ser maiores que zero"
+  -- Se nenhuma das condições acima for verdadeira, realiza o crossover.
+  | otherwise = crossover
     where
+        -- A função 'crossover' não recebe nenhum argumento.
+        -- Ela retorna um par de novos indivíduos que são o resultado do crossover de um ponto aleatório entre o pai e a mãe.
         crossover :: IO (Individuo (*), Individuo (*))
         crossover = do
+            -- Gera um número aleatório entre 0 e o tamanho dos genes do pai menos 1.
             numero_aleatorio <- randomInt (0, length (genes pai) - 1)
+            -- Divide a lista de genes do pai no ponto aleatório.
             let (pai_1, pai_2) = splitAt numero_aleatorio (genes pai)
+            -- Divide a lista de genes da mãe no ponto aleatório.
             let (mae_1, mae_2) = splitAt numero_aleatorio (genes mae)
-
-
+            -- Retorna um par de novos indivíduos. O primeiro indivíduo tem a primeira parte dos genes da mãe e a segunda parte dos genes do pai.
+            -- O segundo indivíduo tem a primeira parte dos genes do pai e a segunda parte dos genes da mãe.
             return (Individuo (mae_1 ++ pai_2) 0, Individuo (pai_1 ++ mae_2) 0)
--- app/Modulos/Mutacao.hs
+++ app/Modulos/Mutacao.hs
@@ -4,19 +4,32 @@ import Tipos (Individuo(genes, Individuo))
 import GerarAleatoriedades (randomFloat, randomInt)
 
 
+-- A função 'mutacao' recebe um indivíduo e uma probabilidade de mutação como entrada.
+-- Ela retorna um novo indivíduo que é o resultado da aplicação da mutação ao indivíduo de entrada.
 mutacao :: Individuo (*) -> Float -> IO (Individuo (*))
 mutacao individuo probabilidade_mutacao = do
+  -- Aplica a função 'mutacao'' aos genes do indivíduo.
   novo_individuo <- mutacao' (genes individuo)
+  -- Retorna um novo indivíduo com os genes mutados e fitness 0.
   return (Individuo novo_individuo 0)
-    where
-        mutacao' :: [*] -> IO [*]
-        mutacao' xs = mapM mutar xs
+  where
+    -- A função 'mutacao'' recebe uma lista de genes e retorna uma nova lista de genes
+    -- onde cada gene foi possivelmente mutado.
+    mutacao' :: [*] -> IO [*]
+    mutacao' xs = mapM mutar xs
 
-        mutar :: * -> IO *
-        mutar item = do
-            prob <- randomFloat (0, 1)
-            if probabilidade_mutacao >= prob then do
-                indice <- randomInt (0, length (genes individuo) - 1)
-                return (genes individuo !! indice)
-            else
-                return item
+    -- A função 'mutar' recebe um gene e retorna um novo gene.
+    -- Com probabilidade 'probabilidade_mutacao', o gene é substituído por um gene aleatório.
+    -- Caso contrário, o gene original é retornado.
+    mutar :: * -> IO *
+    mutar item = do
+      -- Gera um número aleatório entre 0 e 1.
+      prob <- randomFloat (0, 1)
+      if probabilidade_mutacao >= prob
+        then do
+          -- Se a probabilidade de mutação for maior ou igual ao número aleatório,
+          -- substitui o gene por um gene aleatório da lista de genes do indivíduo.
+          indice <- randomInt (0, length (genes individuo) - 1)
+          return (genes individuo !! indice)
+        else -- Caso contrário, retorna o gene original.
+          return item
