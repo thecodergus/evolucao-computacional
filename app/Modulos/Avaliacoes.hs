adce95530fcba66f69ac1eab3ecced4fd0a66f35
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-01 15:54:45 -0300

    Reduzindo a complexidade do algoritmo de avaliação do SAT

--- app/Modulos/Avaliacoes.hs
+++ app/Modulos/Avaliacoes.hs
@@ -6,19 +6,19 @@ import Control.Parallel.Strategies (parMap, rpar)
 -- avaliarSAT recebe um indivíduo booleano e uma lista de disjunções (representadas por listas de inteiros)
 -- e retorna o indivíduo booleano com uma nova avaliação de aptidão (fitness)
 avaliarSAT :: Individuo Bool -> [[Int]] -> Individuo Bool
-avaliarSAT individuo disjuncao = Individuo (genes individuo) $ avaliar $ contaBools $ map or $ replaceWithBools disjuncao $ genes individuo
+avaliarSAT individuo disjuncao = Individuo (genes individuo) $ avaliar $ contaBools $ replaceWithBools disjuncao $ genes individuo
     where
         -- avaliar calcula a aptidão (fitness) como a razão entre a quantidade de Trues e o total de valores
         avaliar :: (Int, Int) -> Float
         avaliar (trueCount, totalCount) = fromIntegral trueCount / fromIntegral totalCount
 
         -- contaBools conta o número de Trues e o total de valores booleanos em uma lista de booleanos
-        contaBools :: [Bool] -> (Int, Int)
+        contaBools :: [[Bool]] -> (Int, Int)
         contaBools = foldl contaBools' (0, 0)
           where
-            contaBools' :: (Int, Int) -> Bool -> (Int, Int)
-            contaBools' (trueCount, totalCount) True = (trueCount + 1, totalCount + 1)
-            contaBools' (trueCount, totalCount) False = (trueCount, totalCount + 1)
+            contaBools' :: (Int, Int) -> [Bool] -> (Int, Int)
+            contaBools' (trueCount, totalCount) uniao | or uniao = (trueCount + 1, totalCount + 1)
+                                                      | otherwise = (trueCount, totalCount + 1)
 
         -- replaceWithBools substitui os inteiros nas listas de disjunções pelos correspondentes valores booleanos
         replaceWithBools :: [[Int]] -> [Bool] -> [[Bool]]
