e9bcd9004728edf797bee55088093b31da9de120
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-03 16:30:52 -0300

    Minha ultima modificação

--- app/Modulos/Selecao.hs
+++ app/Modulos/Selecao.hs
@@ -4,18 +4,33 @@ import Tipos (Individuo(fitness, Individuo), Populacao)
 import Aleatoriedades (randomFloat)
 import Data.List(sort)
 
-roletaViciada :: Populacao (*) -> IO (Populacao (*))
+roletaViciada :: Populacao a -> IO (Populacao a)
 roletaViciada pop = do
+    -- Gerando valor limite 
     valorAleatorio <- randomFloat (0, 1)
     let valorAleatorio = valorAleatorio * somarFitness
 
-    return $ selecionar valorAleatorio 0 pop
+    -- Criando população intermediaria
+    let poplacaIntermediaria = selecionar valorAleatorio 0 pop
+
+    -- Gerando valor limite da população intermediaria
+    valorAleatorio <- randomFloat (0, 1)
+    let valorAleatorio = valorAleatorio * somarFitness
+
+    return $ selecionar valorAleatorio 0 poplacaIntermediaria
         where
+            -- Somando todos os valores das fitness
             somarFitness :: Float
             somarFitness = foldl (\b acc -> b + fitness acc) 0.0 pop
 
-            selecionar :: Float -> Float -> Populacao * -> Populacao *
+            -- Faz a seleção de individuos
+            selecionar :: Float -> Float -> Populacao a -> Populacao a
             selecionar _ _ [] = []
-            selecionar valorAleatorio contador todos@(indi:individuos)  | contador > valorAleatorio =  []
-                                                                        | contador > fitness indi = selecionar valorAleatorio contador individuos
-                                                                        | otherwise = indi : selecionar valorAleatorio (contador + fitness indi) todos
+            selecionar valorAleatorio contador todos = selecionar' valorAleatorio contador todos []
+                where
+                    selecionar' :: Ord a => Float -> Float -> Populacao a -> Populacao a -> Populacao a
+                    selecionar' valorAleatorio contador todos@(indi:individuos) selecionados
+                                | contador > valorAleatorio = selecionados
+                                | length (filter (== indi) selecionados) > 2 = selecionar' valorAleatorio contador individuos selecionados
+                                | contador > fitness indi = selecionar' valorAleatorio contador individuos selecionados
+                                | otherwise = selecionar' valorAleatorio (contador + fitness indi) todos (selecionados ++ [indi])
