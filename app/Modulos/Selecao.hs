53fe3b3b4ae090fe7641ac5872f4b78e55edc123
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-05 18:01:01 -0300

    Arrumando roleta viciada

--- app/Modulos/Selecao.hs
+++ app/Modulos/Selecao.hs
@@ -2,36 +2,36 @@
 {-# HLINT ignore "Eta reduce" #-}
 module Selecao where
 
-import Tipos (Individuo(fitness, Individuo), Populacao)
+import Tipos (Individuo(fitness, Individuo, genes), Populacao)
 import Aleatoriedades (randomFloat)
 import Data.List(sort)
 
 
 
-roletaViciada :: Populacao a -> IO (Populacao a)
+roletaViciada :: Eq a => Populacao a -> IO (Populacao a)
 roletaViciada pop = do
     -- Gerando valor limite 
     valorAleatorio <- randomFloat (0, 1)
     let valorAleatorio = valorAleatorio * somarFitness
 
     -- Criando população Intermediária
-    let popIntermediaria = selecao valorAleatorio 0 pop
+    let popIntermediaria = selecao valorAleatorio 0 pop []
 
     -- Gerando novo valor limite
     valorAleatorio2 <- randomFloat (0, 1)
     let valorAleatorio2 = valorAleatorio2 * somarFitness
 
     -- Criando população final
-    let popFinal = selecao valorAleatorio2 0 popIntermediaria
+    let popFinal = selecao valorAleatorio2 0 popIntermediaria []
 
     return popFinal
     where
         somarFitness :: Float
         somarFitness = foldl (\b acc -> b + fitness acc) 0.0 pop
 
-        selecao :: Float -> Float -> Populacao a -> Populacao a
-        selecao _ _ [] = []
-        selecao valorLimite contador todos@(um:resto)
-            | contador > valorLimite = []
-            | length (filter (\x -> fitness x == fitness um) todos) > 1 = selecao valorLimite (contador + fitness um) resto
-            | otherwise = um : selecao valorLimite (contador + fitness um) todos
+        selecao :: Eq a => Float -> Float -> Populacao a -> Populacao a -> Populacao a
+        selecao _ _ [] _ = []
+        selecao valorLimite contador todos@(um:resto) pop
+            | contador > valorLimite = pop
+            | length (filter (\x -> genes x == genes um) pop) > 1 = selecao valorLimite contador resto (um:pop)
+            | otherwise = selecao valorLimite (contador + fitness um) todos pop
