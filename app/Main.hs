65d1020e46328f86c1146cae56d2105afda8b892
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-09 15:22:51 -0300

    Ajustando main

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,18 @@ module Main where
 
 import Avaliacoes.Matematica (fitnessMax, fitnessMin)
 import Aleatoriedades (randomBoolLista)
+import Arquivo (fileToIntLists)
+import GerarPopulacao (gerarPopulacaoBooleana)
+import RotinaEvolutiva (loopEvolutivoEnumerado)
+import Avaliacoes.Sat (avaliarSAT)
 
 
 
 main :: IO ()
 main = do
-    print "Hello, Haskell!"
-    -- inputBinary <- randomBoolLista 16
-    -- putStrLn $ "Fitness para maximização: " ++ show (fitnessMax inputBinary)
-    -- putStrLn $ "Fitness para minimização: " ++ show (fitnessMin inputBinary)
+    arquivo <- fileToIntLists "/home/udesc/Documentos/evolucao-computacional-main/arquivoSAT.cnf"
+    pop_incial <- gerarPopulacaoBooleana 30 100
+
+    (pop_final, historico) <- loopEvolutivoEnumerado pop_incial (`avaliarSAT` arquivo) 0.05 500
+
+    print historico
