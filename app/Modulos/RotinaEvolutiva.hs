e3b1520e220f8163c9257452c224b82fb9d8a7fd
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-09 14:39:18 -0300

    Terminando rotina evolutiva

--- app/Modulos/RotinaEvolutiva.hs
+++ app/Modulos/RotinaEvolutiva.hs
@@ -7,14 +7,16 @@ import Tipos(Populacao, Individuo)
 import Avaliacoes.Utils(melhorIndividuo)
 import Selecao(roletaViciada)
 import Control.Parallel.Strategies (parMap, rpar)
+import Data.Maybe (maybeToList)
 
 evolutivaEnumerada :: Ord a => Populacao a -> (Individuo a -> Individuo a) -> Float -> Int -> IO (Populacao a)
+evolutivaEnumerada populacao _ _ 0 = return populacao
 evolutivaEnumerada populacao funcaoAvaliacao taxaMutacao contador = do
     -- Avaliacao
     let populacaoAvaliada = parMap rpar funcaoAvaliacao populacao
 
     -- Encontrar o melhor individuo
-    let individuoEletista = melhorIndividuo populacao
+    let individuoEletista = maybeToList $ melhorIndividuo populacaoAvaliada
 
     -- Selecao
     selecaoIndividuos <- roletaViciada populacao
@@ -23,8 +25,8 @@ evolutivaEnumerada populacao funcaoAvaliacao taxaMutacao contador = do
     novaPopulacao <- crossover selecaoIndividuos crossoverUmPontoAleatorio
 
     -- Mutacao
-    let novaPopulacao = parMap rpar (mutacao taxaMutacao) novaPopulacao
+    let novaPopulacao' = parMap rpar (`mutacao` taxaMutacao) novaPopulacao
+    novaPopulacao'' <- sequence novaPopulacao'
 
-    return populacao
 
-
+    evolutivaEnumerada (individuoEletista ++ novaPopulacao'') funcaoAvaliacao taxaMutacao (contador - 1)
