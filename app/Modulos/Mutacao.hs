303a6fd093b7f48c9700d59a0ad120124ca476d8
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-04-22 15:08:18 -0300

    Adicionando mutação bitflip

--- app/Modulos/Mutacao.hs
+++ app/Modulos/Mutacao.hs
@@ -1,4 +1,4 @@
-module Mutacao where
+module Mutacao where 
 
 import Tipos (Individuo(genes, Individuo))
 import Utils.Aleatoriedades (randomFloat, randomInt)
@@ -31,3 +31,30 @@ mutacao individuo probabilidade_mutacao = do
           return (genes individuo !! indice)
         else -- Caso contrário, retorna o gene original.
           return item
+
+-- A função bitflip realiza a mutação bit flip em um cromossomo binário de um indivíduo
+-- Ela recebe um indivíduo do tipo Individuo Bool e uma probabilidade de mutação
+-- e retorna um novo indivíduo com o cromossomo mutado
+bitflip :: Individuo Bool -> Float -> IO (Individuo Bool)
+bitflip (Individuo gene _) probabilidade = do
+  -- Realiza a mutação do gene usando a função mutar
+  genes' <- mutar gene
+  -- Retorna um novo indivíduo com o cromossomo mutado e aptidão definida como 0.0
+  return $ Individuo genes' 0.0
+  where
+    -- A função mutar percorre o cromossomo do indivíduo e decide se cada bit deve ser mutado ou não
+    mutar :: [Bool] -> IO [Bool]
+    -- Caso base: se o cromossomo estiver vazio, retorna uma lista vazia
+    mutar [] = return []
+    -- Caso recursivo: processa o primeiro bit e depois chama mutar nos bits restantes
+    mutar (x : xs) = do
+      -- Gera um número aleatório entre 0 e 1
+      prob <- randomFloat (0, 1)
+      -- Chama mutar nos bits restantes
+      resto <- mutar xs
+      -- Se a probabilidade gerada for menor ou igual à probabilidade de mutação,
+      -- inverte o bit atual e adiciona aos bits restantes
+      if prob <= probabilidade
+        then return $ not x : resto
+        else -- Senão, mantém o bit atual e adiciona aos bits restantes
+          return $ x : resto
