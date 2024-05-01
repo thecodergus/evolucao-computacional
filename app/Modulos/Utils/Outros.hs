8d540ee85e805b4aee33b2e196ebba869458c9d3
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-04-30 21:26:30 -0300

    Comentando a função

--- app/Modulos/Utils/Outros.hs
+++ app/Modulos/Utils/Outros.hs
@@ -2,10 +2,14 @@ module Outros where
 
 
 
+-- Função que troca dois elementos de uma lista em posições específicas
 swapElementsAt :: Int -> Int -> [a] -> [a]
-swapElementsAt i j xs = let elemI = xs !! i
-                            elemJ = xs !! j
-                            left = take i xs
-                            middle = take (j - i - 1) (drop (i + 1) xs)
-                            right = drop (j + 1) xs
-                    in  left ++ [elemJ] ++ middle ++ [elemI] ++ right
+swapElementsAt i j xs =
+  -- Recupera os elementos a serem trocados e as partes da lista antes e depois deles
+  let elemI = xs !! i
+      elemJ = xs !! j
+      left = take i xs
+      middle = take (j - i - 1) (drop (i + 1) xs)
+      right = drop (j + 1) xs
+   in -- Concatena as partes da lista com os elementos trocados
+      left ++ [elemJ] ++ middle ++ [elemI] ++ right
