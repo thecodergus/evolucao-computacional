edeb57f3614587cdfce45564ef19374b8c95b237
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-05 12:14:27 -0300

    Fazendo a função de avaliação das N Rainhas

--- app/Modulos/Avaliacoes/NRainhas.hs
+++ app/Modulos/Avaliacoes/NRainhas.hs
@@ -1,1 +1,19 @@
 module Avaliacoes.NRainhas where
+import Tipos (Individuo (Individuo))
+import Utils.Aleatoriedades (randomFloat)
+
+
+
+avaliacao :: Int -> Individuo Int -> Individuo Int
+avaliacao n (Individuo gene _) = Individuo gene fitness
+    where
+        fitness :: Float
+        fitness = fromIntegral (n - length (rainhasAtacando gene))
+
+        rainhasAtacando :: [Int] -> [(Int, Int)]
+        rainhasAtacando [] = []
+        rainhasAtacando (x : xs) = rainhasAtacando xs ++ rainhasAtacandoNaLinha x xs
+
+        rainhasAtacandoNaLinha :: Int -> [Int] -> [(Int, Int)]
+        rainhasAtacandoNaLinha _ [] = []
+        rainhasAtacandoNaLinha x (y:ys) = if abs (x - y) == abs (length ys) then (x, y) : rainhasAtacandoNaLinha x ys else rainhasAtacandoNaLinha x ys
