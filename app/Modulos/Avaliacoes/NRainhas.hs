e95ed96e9a7023fa2c6d5d0848e4b7390e5e822c
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-05 12:30:50 -0300

    Corrigindo a função de avaliação

--- app/Modulos/Avaliacoes/NRainhas.hs
+++ app/Modulos/Avaliacoes/NRainhas.hs
@@ -1,7 +1,5 @@
 module Avaliacoes.NRainhas where
 import Tipos (Individuo (Individuo))
-import Utils.Aleatoriedades (randomFloat)
-
 
 
 -- Esta função avalia um individuo no problema das N rainhas,
@@ -11,18 +9,22 @@ avaliacao n (Individuo gene _) = Individuo gene fitness
   where
     -- A aptidão (fitness) é calculada a partir da quantidade de rainhas não atacando entre si.
     fitness :: Float
-    fitness = fromIntegral (n - length (rainhasAtacando gene)) / fromIntegral n
+    fitness = fromIntegral (n * (n - 1) `div` 2 - length (rainhasAtacando gene))
+
+    -- Esta função verifica as rainhas que estão se atacando na mesma linha.
+    rainhasAtacandoNaLinha :: [Int] -> Int -> [(Int, Int)]
+    rainhasAtacandoNaLinha xs y = [(i, y) | (i, x) <- zip [0 ..] xs, x == y]
+
+    -- Esta função verifica as rainhas que estão se atacando na mesma coluna.
+    rainhasAtacandoNaColuna :: [Int] -> [(Int, Int)]
+    rainhasAtacandoNaColuna xs = [(i, j) | (i, x) <- zip [0 ..] xs, (j, y) <- zip [0 ..] xs, i /= j, x == y]
+
+    -- Esta função verifica as rainhas que estão se atacando na mesma diagonal.
+    rainhasAtacandoNaDiagonal :: [Int] -> [(Int, Int)]
+    rainhasAtacandoNaDiagonal xs = [(i, j) | (i, x) <- zip [0 ..] xs, (j, y) <- zip [0 ..] xs, i /= j, abs (x - y) == abs (i - j)]
 
     -- Esta função verifica as rainhas que estão se atacando no tabuleiro.
     rainhasAtacando :: [Int] -> [(Int, Int)]
-    rainhasAtacando [] = []
-    rainhasAtacando (x : xs) = rainhasAtacando xs ++ rainhasAtacandoNaLinha x xs
-
-    -- Esta função verifica as rainhas que estão se atacando na mesma linha.
-    rainhasAtacandoNaLinha :: Int -> [Int] -> [(Int, Int)]
-    rainhasAtacandoNaLinha _ [] = []
-    rainhasAtacandoNaLinha x (y : ys) =
-      -- Se a diferença absoluta entre as posições das rainhas for igual à distância entre elas, elas estão se atacando.
-      if abs (x - y) == abs (length ys)
-        then (x, y) : rainhasAtacandoNaLinha x ys
-        else rainhasAtacandoNaLinha x ys
+    rainhasAtacando xs = concat [rainhasAtacandoNaLinha xs y | y <- [0 .. n' - 1]] ++ rainhasAtacandoNaColuna xs ++ rainhasAtacandoNaDiagonal xs
+        where
+            n' = length xs
