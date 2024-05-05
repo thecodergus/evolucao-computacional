ff58bd264a0ae648ccac7ea70e77f731dbceb6e6
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-05 12:06:04 -0300

    Ajustando uns detalhes

--- app/Modulos/Tipos.hs
+++ app/Modulos/Tipos.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE InstanceSigs #-}
 module Tipos where
 
 
 data Individuo a = Individuo
   { genes :: [a],
     fitness :: Float
-  } deriving (Show, Eq)
+  } deriving (Show)
+
+instance Ord a => Ord (Individuo a) where
+  compare :: Ord a => Individuo a -> Individuo a -> Ordering
+  compare i1 i2 = compare (fitness i1) (fitness i2)
+
+instance (Eq a) => Eq (Individuo a) where
+  (==) :: Eq a => Individuo a -> Individuo a -> Bool
+  (Individuo g1 _) == (Individuo g2 _) = g1 == g2
+  (/=) :: Eq a => Individuo a -> Individuo a -> Bool
+  (Individuo g1 _) /= (Individuo g2 _) = g1 /= g2
 
 type Populacao a = [Individuo a]
 
