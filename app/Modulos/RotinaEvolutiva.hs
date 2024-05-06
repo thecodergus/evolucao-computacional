f01bc3837e8b4a454054c8a47f9ec0cd6ebeab1e
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-05-06 18:08:23 -0300

    Ajustando a rotina evolutiva a minha nova vontade

--- app/Modulos/RotinaEvolutiva.hs
+++ app/Modulos/RotinaEvolutiva.hs
@@ -1,8 +1,7 @@
 module RotinaEvolutiva where
 
 
-import Mutacao(mutacao)
-import Crosssover(umPontoAleatorio, crossover)
+import Crosssover(selecionarPais)
 import Tipos (GeracaoInfo (GeracaoInfo, elitistas, mediaFitness), Individuo (Individuo), Populacao)
 import Control.Parallel.Strategies (parMap, rpar)
 import Data.Maybe (maybeToList)
@@ -23,15 +22,18 @@ loopEvolutivoEnumerado populacao funcaoAvaliacao funcaoSelecao funcaoMutacao fun
 
     -- Ativando a questão do generatioGap
     (veios, novinhos) <- selecionarQuemFica generatioGap populacaoAvaliada
-    
+
     -- Selecao
     individuosSelecionados <- funcaoSelecao novinhos
 
+    -- Selecionar pais para fazer crossover
+    pais <- selecionarPais individuosSelecionados
+
     -- Crossover
-    novaPopulacao <- crossover individuosSelecionados funcaoCrossover probabilidadeCrossover
+    novaPopulacao <- fazerCrossover pais
 
     -- Mutacao
-    novaPopulacao' <- mutarPopulacao novaPopulacao
+    novaPopulacao' <- mutarPopulacao (concatMap (\(pai, mae) -> [pai, mae]) novaPopulacao)
 
     -- Ordernar nova interação no Loop evolutivo
     proximaGeracao <- loopEvolutivoEnumerado (individuoEletista ++ novaPopulacao') funcaoAvaliacao funcaoSelecao funcaoMutacao funcaoCrossover probabilidadeCrossover generatioGap (contador - 1)
@@ -45,6 +47,10 @@ loopEvolutivoEnumerado populacao funcaoAvaliacao funcaoSelecao funcaoMutacao fun
         calcularMediaFitness :: Populacao a -> Float
         calcularMediaFitness pop = foldl (\acc (Individuo _ fitness') -> acc + fitness' ) 0.0 pop / fromIntegral (length pop)
 
+        -- Função auxiliar para fazer crossover na população
+        -- fazerCrossover :: [(Individuo a, Individuo a)] -> IO [(Individuo a, Individuo a)]
+        fazerCrossover pais = sequence $ parMap rpar (uncurry funcaoCrossover) pais
+
         -- Função auxiliar para calcular o fitness de uma população de forma paralela
         avaliarPopoulacao = parMap rpar funcaoAvaliacao
 
@@ -56,5 +62,5 @@ loopEvolutivoEnumerado populacao funcaoAvaliacao funcaoSelecao funcaoMutacao fun
         selecionarQuemFica 1 pop = return ([], pop)
         selecionarQuemFica gap pop = do
             pop' <- shuffle pop
-            
+
             return $ splitAt (round $ gap * fromIntegral (length pop)) pop'
