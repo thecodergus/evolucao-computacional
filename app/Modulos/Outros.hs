7a8983c595e0310c188af3535746f0cc137c91ee
Gustavo Michels de Camargo <guga.power@hotmail.com>
2024-05-06 18:56:33 -0300

    Adicionando função para selecionar e remover item de uma lista

--- app/Modulos/Outros.hs
+++ app/Modulos/Outros.hs
@@ -1,38 +1,53 @@
-module Outros where
+module Utils.Outros where
 
-import Tipos (Individuo (Individuo), Populacao)
+import Control.Monad ( forM )
+import Data.Array.IO
+    ( newListArray, readArray, writeArray, IOArray )
+import System.Random ( randomRIO )
+import Utils.Aleatoriedades (randomInt)
 
 
--- Função para realizar escalonamento linear em um indivíduo de uma população
-escalonamentoLinear :: Float -> Populacao a -> Individuo a -> Float
-escalonamentoLinear c
-  | 1.2 <= c && c <= 2.0 = escalonamentoLinear' -- O parâmetro c deve estar entre 1.2 e 2.0
-  | otherwise = error "O valor de c deve estar entre 1.2 e 2.0"
-   where
-      -- A função auxiliar que realiza o escalonamento linear
-      escalonamentoLinear' :: Populacao a -> Individuo a -> Float
-      escalonamentoLinear' pop (Individuo _ fit) = alfa * fit + beta
-         where
-            -- Encontra o valor mínimo de fitness da população
-            fMin :: Float
-            fMin = minimum $ map (\(Individuo _ f) -> f) pop
+-- Função que troca dois elementos de uma lista em posições específicas
+swapElementsAt :: Int -> Int -> [a] -> [a]
+swapElementsAt i j xs =
+  -- Recupera os elementos a serem trocados e as partes da lista antes e depois deles
+  let elemI = xs !! i
+      elemJ = xs !! j
+      left = take i xs
+      middle = take (j - i - 1) (drop (i + 1) xs)
+      right = drop (j + 1) xs
+   in -- Concatena as partes da lista com os elementos trocados
+      left ++ [elemJ] ++ middle ++ [elemI] ++ right
 
-            -- Encontra o valor máximo de fitness da população
-            fMax :: Float
-            fMax = maximum $ map (\(Individuo _ f) -> f) pop
 
-            -- Encontra o valor médio de fitness da população
-            fAvg :: Float
-            fAvg = sum (map (\(Individuo _ f) -> f) pop) / fromIntegral (length pop)
 
-            -- Calcula o valor de alfa para o escalonamento linear
-            alfa :: Float
-            alfa
-               | fMin > ((c * fAvg) - fMax) / c - 1 = (c - 1) * fAvg / (fMax - fAvg)
-               | otherwise = fAvg / (fAvg - fMin)
+-- Função que embaralha uma lista qualquer
+shuffle :: [a] -> IO [a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
 
-            -- Calcula o valor de beta para o escalonamento linear
-            beta :: Float
-            beta
-               | fMin > ((c * fAvg) - fMax) / c - 1 = fAvg * (fMax - (c * fAvg)) / (fMax - fAvg)
-               | otherwise = (-fMin) * fAvg / (fAvg - fMin)
+    -- Cria um array com os elementos da lista
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n' = newListArray (1,n')
+
+
+selecionarRemover :: [a] -> IO (a, [a])
+selecionarRemover itens = do
+  random <- randomInt (0, tam - 1)
+
+  return (itens !! random, removeAt random itens)
+  where
+    tam :: Int
+    tam = length itens
+
+    removeAt :: Int -> [a] -> [a]
+    removeAt n xs = let (as, bs) = splitAt n xs in as ++ tail bs
+
