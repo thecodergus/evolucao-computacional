4d1502a9498f3224a42b20790eed884ece12442f
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-09 21:43:19 -0300

    adicionando as tipagens

--- app/Modulos/Avaliacoes/Radio.hs
+++ app/Modulos/Avaliacoes/Radio.hs
@@ -1,63 +1,49 @@
-module Avaliacoes.Radio where 
+module Avaliacoes.Radio where
 import Tipos (Individuo (Individuo))
+import Debug.Trace(trace)
 
 
--- Esta função realiza a avaliação de um indivíduo com base em seu gene.
--- Ela retorna um novo indivíduo com o mesmo gene, mas com o valor de fitness calculado.
-avaliacao :: Individuo Bool -> Individuo Bool
+-- | Função 'avaliacao' recebe um 'Individuo' contendo um gene de tipo 'Bool' e retorna um 'Maybe' contendo um novo 'Individuo' com o valor de aptidão calculado.
+-- A função verifica se o tamanho do gene é igual a 10. Caso contrário, retorna 'Nothing'.
+-- Caso o tamanho seja válido, o gene é dividido em duas partes, 'st' e 'lx', cada uma contendo 5 elementos.
+-- Os valores de 'st' e 'lx' são convertidos de 'Bool' para 'Float' e divididos pelos valores de 'divSt' e 'divLx', respectivamente.
+-- O valor de aptidão é calculado chamando a função 'fitness' com os valores de 'st' e 'lx'.
+-- O novo 'Individuo' com o gene e o valor de aptidão é retornado dentro de um 'Just'.
+avaliacao :: Individuo Bool -> Maybe (Individuo Bool)
 avaliacao (Individuo gene _)
-  -- Se o tamanho do gene não for 10, uma mensagem de erro é exibida.
-  | length gene /= 10 = error "O gene deve ter 10 bits obrigatoriamente!"
+  | length gene /= 10 = Nothing
   | otherwise = do
-      -- Divide o gene em dois grupos de 5 bits cada.
       let (st, lx) = splitAt 5 gene
-
-      -- Converte o grupo 'st' de bits para float e divide pelo divisor 'divSt'.
-      let st' = boolToFloat st / divSt
-      -- Converte o grupo 'lx' de bits para float e divide pelo divisor 'divLx'.
-      let lx' = boolToFloat lx / divLx
-
-    -- Retorna um novo indivíduo com o mesmo gene, mas com o valor de fitness calculado.
-      Individuo gene (fitness st' lx')
+          st' = boolToFloat st / divSt
+          lx' = boolToFloat lx / divLx
+          fitnessValue = fitness st' lx'
+      Just $ Individuo gene fitnessValue
   where
-
-    -- Divisor utilizado para a conversão do grupo 'st'.
     divSt :: Float
     divSt = 24 / 5
 
-    -- Divisor utilizado para a conversão do grupo 'lx'.
     divLx :: Float
     divLx = 16 / 5
 
-    -- Converte uma lista de booleanos em um número float.
     boolToFloat :: [Bool] -> Float
-    boolToFloat = foldr (\x acc -> (if x then 1 else 0) + (-2) * acc) 0
-
-    -- Função objetivo que calcula o valor baseado nos parâmetros 'st' e 'lx'.
+    boolToFloat = foldr (\x acc -> (if x then 1 else 0) + (-2) * acc) 0 
+    
     funcaoObjetivo :: Float -> Float -> Float
     funcaoObjetivo st lx = 30 * st + 40 * lx
 
-    -- Verifica se os parâmetros 'st' e 'lx' atendem às restrições do problema.
     restricao :: Float -> Float -> Bool
     restricao st lx = st + 2 * lx <= 40 && 0 <= st && st <= 24 && 0 <= lx && lx <= 16
 
     fitness :: Float -> Float -> Float
     fitness st lx
-      -- Calcula o valor de fitness com base na função objetivo e nas restrições.
       | restricao st lx = foN + r * hN
-        -- Se as restrições não forem atendidas, o valor de fitness é zero.
       | otherwise = 0
       where
-
-        -- Penalização aplicada quando há violação das restrições.
         r :: Float
         r = -1
 
-        -- Valor normalizado da função objetivo.
         foN :: Float
         foN = funcaoObjetivo st lx / 1360
 
-        -- Valor normalizado da violação das restrições.
         hN :: Float
-        hN = maximum [0 .. ((st + 2 * lx - 40) / 16)]
-
+        hN = max 0 ((st + 2 * lx - 40) / 16)
