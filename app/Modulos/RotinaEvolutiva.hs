f91528ba7b5e66f9d09bb26496bb5eb88d01413d
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-10 18:33:55 -0300

    Revert "Melhorando a função que seleciona quem fica e quem sai"
    This reverts commit 8fca1a148e1882c8cbea3cebfdce9f5fec00a6ae.

--- app/Modulos/RotinaEvolutiva.hs
+++ app/Modulos/RotinaEvolutiva.hs
@@ -8,10 +8,8 @@ import Control.Parallel.Strategies (parMap, rpar)
 import Data.Maybe (maybeToList)
 import Utils.Avaliacoes (melhorIndividuo)
 import Utils.Outros (shuffle)
-import Utils.Aleatoriedades (selecionarRemoverRandom, randomIntLista)
+import Utils.Aleatoriedades (selecionarRemoverRandom)
 import Debug.Trace(trace)
-import Data.List (sort)
-import Data.Either (partitionEithers)
 
 -- Retorna a ultima População e o historico de melhores individuos
 loopEvolutivoEnumerado :: (Ord a, Show a) => 
@@ -75,18 +73,9 @@ loopEvolutivoEnumerado populacao funcaoAvaliacao funcaoSelecao funcaMutacao func
         selecionarQuemFica :: Float -> Populacao a -> IO (Populacao a, Populacao a)
         selecionarQuemFica 1 pop = return ([], pop)
         selecionarQuemFica gap pop = do
-            quemFica <- randomIntLista (0, length pop - 1) (round $ gap * fromIntegral (length pop))
+            pop' <- shuffle pop
 
-
-            return $ partitionEithers $ selecionar quemFica (zip pop [0 ..])
-                where
-                -- Função que seleciona elementos com base em índices
-                selecionar :: [Int] -> [(Individuo a, Int)] -> [Either (Individuo a) (Individuo a)]
-                selecionar [] _ = []
-                selecionar _ [] = []
-                selecionar indices ((individuo, index) : xs)
-                    | index `elem` indices = Left individuo : selecionar indices xs
-                    | otherwise = Right individuo : selecionar indices xs
+            return $ splitAt (round $ gap * fromIntegral (length pop)) pop'
 
         -- Função auxiliar para selecionar os pais e realizar o crossover entre eles
         crossover :: Populacao a -> ((Individuo a, Individuo a) -> IO (Individuo a, Individuo a)) -> Int -> IO (Populacao a)
