591259954f877261eea69131e7659b06e11c187b
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-05-11 17:07:35 -0300

    Mudando função de fitness

--- app/Modulos/Avaliacoes/NRainhas.hs
+++ app/Modulos/Avaliacoes/NRainhas.hs
@@ -9,7 +9,7 @@ avaliacao n (Individuo gene _) = Individuo gene fitness
   where
     -- A aptidão (fitness) é calculada a partir da quantidade de rainhas não atacando entre si.
     fitness :: Float
-    fitness = fromIntegral (n * (n - 1) `div` 2 - length (rainhasAtacando gene))
+    fitness = 1 - (fromIntegral (length (rainhasAtacando gene)) / fromIntegral (length gene))
 
     -- Esta função verifica as rainhas que estão se atacando no tabuleiro.
     rainhasAtacando :: [Int] -> [(Int, Int)]
