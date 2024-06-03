1dba2e1f3ad37046ff6b30b730460f19e759aa9d
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-03 14:37:09 -0300

    Melhorando o codigo

--- app/Modulos/Avaliacoes/NRainhasOtmizado.hs
+++ app/Modulos/Avaliacoes/NRainhasOtmizado.hs
@@ -6,12 +6,12 @@ import qualified Avaliacoes.NRainhas as NRainhas
 avaliacao :: Int -> Individuo Int -> Individuo Int
 avaliacao n (Individuo gene _)
   | length gene /= n = error "O tamanho do gene é diferente do tamanho do tabuleiro."
-  | otherwise = NRainhas.avaliacao n (Individuo (tratamento gene) 0)
+  | otherwise = NRainhas.avaliacao n (Individuo (tratamento n gene) 0)
   where
-    tratamento :: [Int] -> [Int]
-    tratamento vetor = zipWith (curry (length vetor `transformar`)) vetor [0 ..]
+    tratamento :: Int -> [Int] -> [Int]
+    tratamento n' vetor = zipWith (curry (n' `transformar`)) vetor [0 ..]
         where
             transformar :: Int -> (Int, Int) -> Int
-            transformar n (a, b)
-                | even (b `mod` n) = (round . sqrt ) (fromIntegral a :: Double)
+            transformar n'' (a, b)
+                | even (b `mod` n'') = (round . sqrt ) (fromIntegral a :: Double)
                 | otherwise = (round . (10 `logBase`)) (fromIntegral a :: Double)
