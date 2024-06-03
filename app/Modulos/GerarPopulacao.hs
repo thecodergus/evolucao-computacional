5cbc54d538264a23ce06a6161eaa0a7f2aafb5e7
PCdaFacul <gustavo.michels.de.camargo@gmail.com>
2024-06-03 14:22:48 -0300

    Alterando função gerar permutado

--- app/Modulos/GerarPopulacao.hs
+++ app/Modulos/GerarPopulacao.hs
@@ -5,6 +5,7 @@ import Tipos (Populacao, Individuo (Individuo))
 import GerarIndividuos (gerarIndividuoBooleano, gerarIndividuoInteiroBound, gerarIndividuoFlutuante, gerarIndividuoInteiroForSAT)
 import Control.Monad (replicateM)
 import Data.List (permutations)
+import System.Random (newStdGen, randomRs)
 
 
 -- Função para gerar uma população de indivíduos booleanos
@@ -15,13 +16,11 @@ gerarPopulacaoBooleana num_individuos num_genes = replicateM num_individuos (ger
 gerarPopulacaoInteiroBound :: Int -> Int -> (Int, Int) -> IO (Populacao Int)
 gerarPopulacaoInteiroBound num_individuos num_genes intervalo = replicateM num_individuos (gerarIndividuoInteiroBound num_genes intervalo)
 
--- Função para gerar uma população de indivíduos inteiros permutados
 gerarPopulacaoInteiroPermutado :: Int -> Int -> (Int, Int) -> IO (Populacao Int)
 gerarPopulacaoInteiroPermutado num_individuos num_genes intervalo
-  | num_individuos > num_genes =  error "Número de indivíduos deve ser menor ou igual ao número de genes"
   | num_individuos < 0 = error "Número de indivíduos deve ser maior que zero"
   | num_genes < 0 = error "Número de genes deve ser maior que zero"
-  | otherwise = return $ map (`Individuo` 0) (take num_individuos (permutations [fst intervalo .. snd intervalo]))
+  | otherwise = newStdGen >>= \gen -> return $ take num_individuos $ map (`Individuo` 0) $ replicateM num_genes (randomRs intervalo gen)
 
 -- Função para gerar uma população de indivíduos flutuantes
 gerarPopulacaoFlutuante :: Int -> Int -> (Float, Float) -> IO (Populacao Float)
