3257a12f94b4c3e976d2a6cdb15688be1b4a9073
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-06 19:10:25 -0300

    Adicionando numero de colisões na conta

--- app/Main.hs
+++ app/Main.hs
@@ -90,7 +90,7 @@ nRainhas ((n, numGeracoes) : ss) = do
 
   gravarHistorico geracaoInfo ("N" ++ show n ++ "-G" ++ show numGeracoes ++ "-" ++ formatTime defaultTimeLocale "%Y-%m-%d_%H-%M-%S" currentTime ++ "-Grafico-NRainhas.roletaSemReposicao.swap.cx.png")
 
-  appendFile ("N" ++ show n ++ "-G" ++ show numGeracoes ++ "-NRainhas.csv") (show n ++ ";" ++ show numGeracoes ++ ";" ++ show (genes $  head $ maybeToList $ Avaliacoes.melhorIndividuo $ melhorIndividuo geracaoInfo) ++ ";" ++ show (Rainhas.fo (genes (fromMaybe (error "Invalid individual") $ Avaliacoes.melhorIndividuo $ melhorIndividuo geracaoInfo)) n) ++ ";" ++ show execTime ++ "\n")
+  appendFile ("N" ++ show n ++ "-G" ++ show numGeracoes ++ "-NRainhas.csv") (show n ++ ";" ++ show numGeracoes ++ ";" ++ show (genes $ head $ maybeToList $ Avaliacoes.melhorIndividuo $ melhorIndividuo geracaoInfo) ++ ";" ++ show (Rainhas.fo (genes (fromMaybe (error "Invalid individual") $ Avaliacoes.melhorIndividuo $ melhorIndividuo geracaoInfo)) n) ++ ";" ++ show execTime ++ ";" ++ show (Rainhas.rainhasAtacando (genes $ head $ maybeToList $ Avaliacoes.melhorIndividuo $ melhorIndividuo geracaoInfo)) ++ "\n")
 
   void (nRainhas ss)
 
@@ -119,16 +119,16 @@ main = nRainhas [
     (32, 20000),
     (32, 20000),
     (32, 20000),
-    (32, 20000),
+    (32, 20000)
 
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000),
-    (64, 20000)
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000),
+    -- (64, 20000)
   ]
