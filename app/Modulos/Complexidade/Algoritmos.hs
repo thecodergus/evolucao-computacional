79ce9ff990aa4d45783691e6e6867138a40787c3
Gustavo Michels de Camargo <gustavo.michels.de.camargo@gmail.com>
2024-06-25 00:02:45 -0300

    Criando os primeiro algoritmos para calular rota total

---
+++ app/Modulos/Complexidade/Algoritmos.hs
@@ -0,0 +1,27 @@
+module Complexidade.Algoritmos where
+
+import Complexidade.Tipos
+import Data.List (nub)
+
+nomesCidades :: [Trajeto] -> [String]
+nomesCidades input = nub $ concatTrajeto input
+    where
+        concatTrajeto :: [Trajeto] -> [String]
+        concatTrajeto [] = [] 
+        concatTrajeto ((Trajeto de para _) : xs) = de : para : concatTrajeto xs
+
+
+descobrirDistancia :: String -> String -> [Trajeto] -> Integer
+descobrirDistancia _ _ [] = 9999999
+descobrirDistancia a b ((Trajeto de para m) : lista)
+    | a == de && b == para = 
+        case m of
+            Just x -> x
+            Nothing -> 9999999
+    | otherwise = descobrirDistancia a b lista
+
+
+somarDistancias :: [String] -> [Trajeto] -> Integer
+somarDistancias [] _ = 0
+somarDistancias _ [] = 0
+somarDistancias (a : b : resto) lista = (descobrirDistancia a b lista) + (somarDistancias (b : resto) lista)
